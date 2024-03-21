dd69f5d08b98eb20def9752b30e1bf8dcab1a18e
belhadj haitam <steankir.haitam@gmail.com>
2024-03-21 22:56:34 +0000

    test2 is done

--- parcing.c
+++ parcing.c
@@ -6,7 +6,7 @@
 /*   By: hbelhadj <hbelhadj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/16 22:54:41 by hbelhadj          #+#    #+#             */
-/*   Updated: 2024/03/21 21:11:45 by hbelhadj         ###   ########.fr       */
+/*   Updated: 2024/03/21 22:55:45 by hbelhadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,9 @@
 //     }
 //     return *(const unsigned char *)s1 - *(const unsigned char *)s2;
 // }
+
+#define TRUE 1
+#define FALSE 0
 
 char	*ft_strdup(char *s)
 {
@@ -106,10 +109,9 @@ int        is_cub(char        *filename)
                 return(printf("invalid name\n"), 0);
         if (ft_strncmp(ext, ".cub", 4) == 0 && ft_strlen(ext) == 4)
         {
-            printf("filename is good\n");
+            printf("TEST 1: good💪🏻\n");
             return (1);
         }
-        // return (printf("invalid file .cub"), 1);
         return 0;
 }
 
@@ -158,11 +160,13 @@ char **read_line(char   *str)
     
     i = 0;
     fd = open(str, O_RDWR);
+    // printf("line_number : %d\n", line_number(str));
     store_first_line = get_next_line(fd);
     store_all_map = malloc(sizeof(char *) * line_number(str) + 1);
     while(store_first_line != 0)
     {
-        store_all_map[i] = strdup(store_first_line);
+        store_all_map[i] = ft_strdup(store_first_line);
+        // printf("=======> %s\n", store_all_map[i]);
         free(store_first_line);
         store_first_line = get_next_line(fd);
         i++;
@@ -202,6 +206,8 @@ char **extract_6(char **file)
         if (!is_all_white)
         {
             extract_6[j++] = ft_strdup(file[i]);
+            // printf("<====== %s =====>\n", extract_6[j++]);
+            // printf("<====== %d =====>\n", j++);
             y++;
         }
         i++;
@@ -216,21 +222,30 @@ char *rest_(char *line, char *str)
     char *rest;//store the extracted rest of the line and the first part of the line, respectively.
     char *store;//store the extracted rest of the line and the first part of the line, respectively.
     char *start;
+
+    (void)str;
     
     i = 0;
     if(!line)
+    {
         return (NULL);
+    }
     //This while loop iterates through the input line until it finds a non-space or non-tab character.
     while(line[i] && (line[i] == ' ' || line[i] == '\t'))
         i++;
     store = strdup(line + 1);//This line uses the strdup function to create a duplicate of the part of the line after skipping leading spaces or tabs. It starts copying from the position indicated by line + i, which is the first non-space or non-tab character
     if(!store)
-        return (NULL);
-    if (strncmp(store, str, ft_strlen(str)) != 0)
     {
-        free(store);
         return (NULL);
     }
+    // if (ft_strncmp(store, str, ft_strlen(str)) != 0)
+    // {
+    //     printf("*********42\n");
+    //     printf("store : %s\n", store);
+    //     printf("str : %s\n", str);
+    //     free(store);
+    //     return (NULL);
+    // }
     start = strchr(store, ' ');
     if (!start)
         start = strchr(store, '\t');
@@ -299,36 +314,47 @@ char *str_(char *str)
 // Define the paths function
 int paths(char **line)
 {
+    paths_struct.pars.path_ea = NULL;
+    paths_struct.pars.path_no = NULL;
+    paths_struct.pars.path_we = NULL;
+    paths_struct.pars.path_so = NULL;
 
     // Extract path associated with "NO"
     char *tmp = rest_(find_(line, "NO"), "NO");
     if (!tmp)
-        return 0;
+    {
+        // printf("ERROR 1337\n");
+        return FALSE;
+    }
     free(tmp);
     paths_struct.pars.path_no = str_(rest_(find_(line, "NO"), "NO"));
+    // printf("%s------>\n", paths_struct.pars.path_no);
 
     // Extract path associated with "SO"
     tmp = rest_(find_(line, "SO"), "SO");
     if (!tmp)
-        return (free(paths_struct.pars.path_no), 0);
+        return (free(paths_struct.pars.path_no), FALSE);
     free(tmp);
     paths_struct.pars.path_so = str_(rest_(find_(line, "SO"), "SO"));
+    // printf("%s------>\n", paths_struct.pars.path_so);
 
     // Extract path associated with "WE"
     tmp = rest_(find_(line, "WE"), "WE");
     if (!tmp)
-        return (free(paths_struct.pars.path_no), free(paths_struct.pars.path_so), 0);
+        return (free(paths_struct.pars.path_no), free(paths_struct.pars.path_so), FALSE);
     free(tmp);
     paths_struct.pars.path_we = str_(rest_(find_(line, "WE"), "WE"));
+    // printf("%s------>\n", paths_struct.pars.path_we);
 
     // Extract path associated with "EA"
     tmp = rest_(find_(line, "EA"), "EA");
     if (!tmp)
         return (free(paths_struct.pars.path_no), free(paths_struct.pars.path_so),
-                free(paths_struct.pars.path_we), 0);
+                free(paths_struct.pars.path_we), FALSE);
     free(tmp);
+    // printf("%s------>\n", paths_struct.pars.path_ea);
     paths_struct.pars.path_ea = str_(rest_(find_(line, "EA"), "EA"));
-    return 1;
+    return TRUE;
 }
 int ex_color(char **line)
 {
@@ -336,40 +362,49 @@ int ex_color(char **line)
 
     tmp = rest_(find_(line, "F"), "F");
     if(!tmp)
-        return (0);
+    {
+        // printf("FFFFFFFFFF 1337");
+        return (FALSE);
+    }
     free(tmp);
     paths_struct.pars.ex_color = str_(rest_(find_(line, "F"), "F"));
     tmp = rest_(find_(line, "C"), "C");
     if(!tmp)
-        return (free(paths_struct.pars.ex_color), 0);
+    {
+
+        // printf("CCCCCCCCC 1337");
+        return (free(paths_struct.pars.ex_color), FALSE);
+    }
+    // printf("ERROR 1337");
     free(tmp);
     paths_struct.pars.ex_color = str_(rest_(find_(line, "C"), "C"));
-    return (1);
+    // printf("ERROR 1337******");
+    return (TRUE);
 }
 
 int check_line(char *str)
 {
     char **file;
     char **line;
-    // t_paths paths_struct;
 
     file = read_line(str);//read the lines of the file and stroed in an array
     line = extract_6(file);
     // free_(file);
+
     if(!paths(line))
-        return (free_(line), 0);
-    if(!paths_struct.pars.path_ea || !paths_struct.pars.path_no || !paths_struct.pars.path_we || !paths_struct.pars.path_so)
-        return (free_(line), 0); 
+        return (free_(line), FALSE);
     if(!ex_color(line))
-        return (free_(line), free(paths_struct.pars.path_ea), free(paths_struct.pars.path_we), free(paths_struct.pars.path_so), free(paths_struct.pars.path_no), 0);   
-    return (free_(line), 1);
+        return (free_(line), free(paths_struct.pars.path_ea), free(paths_struct.pars.path_we), free(paths_struct.pars.path_so), free(paths_struct.pars.path_no), FALSE);
+    printf("TEST 2: good💪🏻\n");
+    return (free_(line), TRUE);
 }
 
 int parsing(char *str)
 {
     if (!is_cub(str))
         return (printf("check '.cub'\n"), 0);
-    else if (!check_line(str))
-        return (printf("check file\n"), 0);
+    if (!check_line(str))
+        return (printf("check file\n"), FALSE);
+    // printf("code error : %d\n", check_line(str));
     return 1;
 }
